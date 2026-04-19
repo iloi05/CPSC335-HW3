9d96a95b752dab38ebc8cd7ffa851a9723b4b335
root <root@DESKTOP-8969P1M.localdomain>
2026-04-18 22:44:13 -0700

    reverse finished

---
+++ listreverse.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <list>
+
+std::list<int> reverse_list(const std::list<int>& lst) {
+   std::list<int> reversed;
+   for (const auto& l : lst) {
+        reversed.insert(reversed.begin(), l);
+   }
+   return reversed;
+}
+
+int main() {
+    std::list<int> lst = {1, 2, 3, 4, 5};
+    std::list<int> reversed_lst = reverse_list(lst);
+
+    std::cout << "Original list: ";
+    for (const auto& l : lst) {
+        std::cout << l << " ";
+    }
+    std::cout << "\nReversed list: ";
+    for (const auto& l : reversed_lst) {
+        std::cout << l << " ";
+    }
+    std::cout << std::endl;
+
+    std::list<int> list2 = {10, 20, 30};
+    std::list<int> reversed_list2 = reverse_list(list2);
+    std::cout << "Original list: ";
+    for (const auto& l : list2) {
+        std::cout << l << " ";
+    }
+    std::cout << "\nReversed list: ";
+    for (const auto& l : reversed_list2) {
+        std::cout << l << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
