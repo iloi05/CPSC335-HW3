9910b7f875a816ca94401811fbc7cefca0630173
root <root@DESKTOP-8969P1M.localdomain>
2026-04-25 14:30:30 -0700

    3a changed to naiive

--- 3a.cpp
+++ 3a.cpp
@@ -8,34 +8,36 @@ CPSC 335 section 10
 #include <list>
 #include <set>
 
+bool has(const std::list<int>& lst, int value) {
+    for (int i : lst) {
+        if (i == value) {
+            return true;
+        }
+    }
+    return false;
+}
+
 std::list<int> LOrR(std::list<int> L, std::list<int> R) {
-    std::set<int> visited;
     std::list<int> S;
 // Iterate through list L and add elements that are not in R to S
     for (int i : L) {
 // If the element i from L is not found in R and has not been added to S,
 // add it to S
-        if (visited.count(i) == 0) {
+        if (!has(R, i)) {
             S.push_back(i);
-// Mark the element i as visited to avoid adding duplicates from R
-            visited.insert(i);
         }
     }
 // Iterate through list R and add elements that are not in L to S
     for (int i : R) {
 // If the element i from R is not found in L and has not been added to S,
 // add it to S
-        if (visited.count(i) == 0) {
+        if (!has(L, i)) {
             S.push_back(i);
-// Mark the element i as visited to avoid adding duplicates
-            visited.insert(i);
         }
     }
 // Return the list S containing elements that are in L or R but not in both
     return S;
-
 }
-
 int main() {
     std::list<int> L = {1, 2, 3, 4, 5};
     std::list<int> R = {4, 5, 6, 7, 8};
