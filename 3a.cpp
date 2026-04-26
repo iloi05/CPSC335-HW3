8ddbbed4a96c13db8d6428bcd151a001303f94be
root <root@DESKTOP-8969P1M.localdomain>
2026-04-25 20:06:48 -0700

    added comments to 3a and 3b

--- 3a.cpp
+++ 3a.cpp
@@ -11,13 +11,18 @@ CPSC 335 section 10
 std::list<int> LOrR(const std::list<int>& L, const std::list<int>& R) {
     std::unordered_set<int> visited;
     std::list<int> S;
-
+    // Add elements from L that are not in R
     for(int l : L) {
+    // Check if any nodes in L have been visited yet
         if (visited.find(l) == visited.end()) {
+    // push into S if not yet
             S.push_back(l);
+    // mark as visited
             visited.insert(l);
         }
     }
+    // Add elements from R that are not in L
+    // Same process as above but for R
     for(int r : R) {
         if (visited.find(r) == visited.end()) {
             S.push_back(r);
--- 3b.cpp
+++ 3b.cpp
@@ -8,9 +8,12 @@ CPSC 335 section 10
 #include <unordered_set>
 
 std::list<int> setDifference(const std::list<int>& L, const std::list<int>& R) {
-    std::unordered_set<int> hashR(R.begin(), R.end()); // Create a hash set from list R for O(1) lookups
+    // Hash the elements of R for lookup
+    std::unordered_set<int> hashR(R.begin(), R.end());
     std::list<int> difference;
+    // Add elements from L that are not in R
     for (int l : L) {
+    // if l is not in R, add to difference
         if (hashR.find(l) == hashR.end()) {
             difference.push_back(l);
         }
