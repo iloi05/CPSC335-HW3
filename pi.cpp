8ecc5266d301e60f3cdc91c0013c5c3925eed320
root <root@DESKTOP-8969P1M.localdomain>
2026-04-25 20:31:50 -0700

    pi file fixed w comments

--- pi.cpp
+++ pi.cpp
@@ -8,12 +8,22 @@ CPSC 335 section 10
 #include <optional>
 
 std::optional<std::tuple<int, int, int>> triplePi(int a, int b) {
+    // Validate input parameters
+    if (a < 1) {
+        std::cout << "Input a must be at least 1." << std::endl;
+        return std::nullopt;
+    }
+    if (b < 1) {
+        std::cout << "Input b must be at least 1." << std::endl;
+        return std::nullopt;
+    }
 // Iterate through all possible combinations of x, y, z in the range [a, b)
-    for (int x = a; x < b; ++x) {
-        for (int y = a; y < b; ++y) {
-            for (int z = a; z < b; ++z) {
-// Check if the sum of squares equals 3
-                if (x * x + y * y + z * z == 3) {
+    for (int x = a; x <= b; ++x) {
+        for (int y = x; y <= b; ++y) {
+            for (int z = y; z <= b; ++z) {
+// Check if the combination (x, y, z) satisfies
+// the Pythagorean triple condition: x^2 + y^2 = z^2
+                if (x * x + y * y == z * z) {
 // If we find such a triple, return it as a tuple
                     return std::make_tuple(x, y, z);
                 }
@@ -24,7 +34,7 @@ std::optional<std::tuple<int, int, int>> triplePi(int a, int b) {
 }
 
 int main() {
-    int a = 0, b = 2;
+    int a = 1, b = 2;
     auto result = triplePi(a, b);
     
     if (result) {
